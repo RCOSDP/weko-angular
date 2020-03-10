7e4d75ebea5c22e13e8255cf008b0c70d3da77e0
weko3-dev23 <weko3-hitachi-dev23@ml.itg.hitachi.co.jp>
2020-03-10 19:06:56 +0700

    Front-End: Add Author Mng ID prefix Scheme

--- app-author-prefix/src/app/app.component.ts
+++ app-author-prefix/src/app/app.component.ts
@@ -17,31 +17,58 @@ export class AppComponent implements OnInit {
       id: -1,
       name: "",
       url: "",
+      scheme: "",
       name_temp: "",
-      url_temp: ""
+      url_temp: "",
+      scheme_temp: ""
     }
   ];
   public new_settings: any = {
     name: "",
-    url: ""
+    url: "",
+    scheme: ""
   };
-    
-  constructor(private http: Http,) { }
+  public controlledVocabularies = [];
+  public otherIndex: number = 0;
+  public schemeOtherValue: string = "";
+  public schemeOtherValue_temp: string = "";
+  public selectedScheme: string = "";
+  public selectedScheme_temp: string = "";
+
+  constructor(private http: Http, ) { }
 
   ngOnInit() {
   }
   ngAfterViewInit() {
     this.getAuthorsPrefixSettings();
+    this.getControlledVocabularies();
+  }
+  /**
+   * edit authors prefix settings
+   */
+  edit(index: any) {
+    this.control.edit_flag[index] = true
+    console.log(this.displayData[index]);
+    this.displayData[index].name_temp = this.displayData[index].name;
+    this.displayData[index].url_temp = this.displayData[index].url;
+    if (this.controlledVocabularies.indexOf(this.displayData[index].scheme) < 0) {
+      this.selectedScheme_temp = this.controlledVocabularies[this.otherIndex];
+      this.schemeOtherValue_temp = this.displayData[index].scheme;
+    } else {
+      this.selectedScheme_temp = this.displayData[index].scheme;
+      this.schemeOtherValue_temp = "";
+    }
   }
   /**
    * update authors prefix settings
    */
   update(index: any) {
     console.log(this.displayData[index]);
-    let data = {id: -1, name: "", url: ""};
+    let data = { id: -1, name: "", url: "", scheme: "" };
     data.id = this.displayData[index].id;
     data.name = this.displayData[index].name_temp;
     data.url = this.displayData[index].url_temp;
+    data.scheme = this.selectedScheme_temp != this.controlledVocabularies[this.otherIndex] ? this.selectedScheme_temp : this.schemeOtherValue_temp;
     let validation_res = this.validation(data);
     if (validation_res != 'OK') {
       alert(validation_res);
@@ -49,12 +76,17 @@ export class AppComponent implements OnInit {
     }
     this.updateSettings(data).then(
       res => {
-        alert('Update completed');
+        if (res.code == 400) {
+          alert(res.msg);
+        } else {
+          alert('Update completed');
+          window.location.reload();
+        }
       }).catch(
-      res => {
-        alert(res.msg);
-    });
-    window.location.reload();
+        res => {
+          alert(res.msg);
+          window.location.reload();
+        });
   }
   /**
    * delete authors prefix settings
@@ -65,15 +97,16 @@ export class AppComponent implements OnInit {
       res => {
         alert('Successfully deleted');
       }).catch(
-      res => {
-        alert(res.msg);
-    });
+        res => {
+          alert(res.msg);
+        });
     window.location.reload();
   }
   /**
    * add authors prefix settings
    */
   add() {
+    this.new_settings.scheme = this.selectedScheme != this.controlledVocabularies[this.otherIndex] ? this.selectedScheme : this.schemeOtherValue;
     console.log(this.new_settings);
     let validation_res = this.validation(this.new_settings);
     if (validation_res != 'OK') {
@@ -82,12 +115,17 @@ export class AppComponent implements OnInit {
     }
     this.putSettings(this.new_settings).then(
       res => {
-        alert('Successfully added');
+        if (res.code == 400) {
+          alert(res.msg);
+        } else {
+          alert('Successfully added');
+          window.location.reload();
+        }
       }).catch(
-      res => {
-        alert(res.msg);
-    });
-    window.location.reload();
+        res => {
+          alert(res.msg);
+          window.location.reload();
+        });
   }
   /**
    * get authors prefix settings
@@ -98,10 +136,23 @@ export class AppComponent implements OnInit {
         this.displayData = res;
         console.log(res);
         for (let i = 0; i < res.length; i++) {
-            this.control.edit_flag[i] = false;
-            this.displayData[i].name_temp = this.displayData[i].name;
-            this.displayData[i].url_temp = this.displayData[i].url;
+          this.control.edit_flag[i] = false;
+          this.displayData[i].name_temp = this.displayData[i].name;
+          this.displayData[i].url_temp = this.displayData[i].url;
+          this.displayData[i].scheme_temp = this.displayData[i].scheme;
         }
+      }
+    ).catch();
+  }
+  /**
+   * get list of controlled vocabularies
+   */
+  getControlledVocabularies() {
+    this.getListOfControlledVocabularies().then(
+      res => {
+        this.controlledVocabularies = res.list;
+        this.otherIndex = res.index;
+        console.log(res);
       }
     ).catch();
   }
@@ -111,6 +162,18 @@ export class AppComponent implements OnInit {
   getDataOfAuthorsPrefixSettings() {
     var urlArr = window.location.href.split('/');
     const url = urlArr[0] + "//" + urlArr[2] + "/api/authors/search_prefix";
+    return this.http
+      .get(url)
+      .toPromise()
+      .then(response => response.json() as any)
+      .catch(this.handleError);
+  }
+  /**
+   * call web api (get list of controlled vocabularies)
+   */
+  getListOfControlledVocabularies() {
+    var urlArr = window.location.href.split('/');
+    const url = urlArr[0] + "//" + urlArr[2] + "/api/authors/list_controlledvocabularies";
     return this.http
       .get(url)
       .toPromise()
@@ -160,8 +223,17 @@ export class AppComponent implements OnInit {
     if (data.name.trim().length == 0) {
       return 'Please enter the correct "Name"';
     }
+    if (data.scheme.trim().length == 0) {
+      return 'Please enter the correct "Scheme"';
+    }
+    if (data.scheme.trim().length > 20) {
+      return '"Scheme" is longer than 20 characters';
+    }
+    if (this.displayData.find(i => i.scheme == data.scheme && i.id != data.id)) {
+      return '"Scheme" must be unique';
+    }
     let reg = /^(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/;
-    if (data.url.length > 0  && !reg.test(data.url)) {
+    if (data.url.length > 0 && !reg.test(data.url)) {
       return 'Please enter the correct "URL"';
     }
     return 'OK';
